63d9bdea4bb4f6ad073ab2d634929eaa17613df8
Mantas <mantasge@gmail.com>
2022-06-05 22:08:37 +0300

    deployment config

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 require("dotenv").config({path: "./src/config/.env"})
+const path = require("path");
 
 const express = require("express");
 const app = express();
@@ -9,6 +10,14 @@ const apiRoutes = require("./src/routes/routes");
 app.use(cors());
 app.use(express.json());
 app.use("/api", apiRoutes);
+
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client/build")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+}
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
